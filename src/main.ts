7f313926764c88cc07324bd8bda11d9241ca1c70
Ilshat Khasanov <kristiansummer01@gmail.com>
2025-01-27 16:17:28 +0300

    https opened

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ const cors = require('cors')
 import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: '/etc/letsencrypt/live/articlesplatform.ru/privkey.pem',
+      cert: '/etc/letsencrypt/live/articlesplatform.ru/fullchain.pem'
+    }
+  });
   // User.sync({alter: true})
   // Article.sync({alter: true})
   app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL
+    origin: process.env.CLIENT_URL,
 }))
   app.use(cookieParser())
   await app.listen(process.env.PORT, () => {
