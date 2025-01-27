46b9787e71f78e65bc500aeeba940536f3175a74
Ilshat Khasanov <kristiansummer01@gmail.com>
2025-01-27 18:41:35 +0300

    key and cert paths fixed

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 const cors = require('cors')
 import * as cookieParser from 'cookie-parser'
 import * as fs from 'fs'
+import path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions: {
-      key: fs.readFileSync('/etc/letsencrypt/live/articlesplatform.ru/privkey.pem', 'utf-8'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/articlesplatform.ru/fullchain.pem', 'utf-8')
+      key: fs.readFileSync(path.join(__dirname, '../etc/letsencrypt/live/articlesplatform.ru/privkey.pem'), 'utf-8'),
+      cert: fs.readFileSync(path.join(__dirname, '../etc/letsencrypt/live/articlesplatform.ru/fullchain.pem'), 'utf-8')
     }
   });
   // User.sync({alter: true})
